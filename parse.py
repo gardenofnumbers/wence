3d269ee6c6aab04e4ba36fec7884f45b1bd40d8c
gardenofnumbers <numbers@loom.wiki>
2022-08-05 02:57:32 -0700

    parsable quicksort algorithm holy shit did I make progress today

--- parse.py
+++ parse.py
@@ -1,344 +1,71 @@
-import string
-import traceback
-
-_depth = 0;
-
-class ASTNode0(object):
-    nodeid_ = 0;
-    def __init__(self, t, v):
-        self.n = ASTNode0.nodeid_;
-        ASTNode0.nodeid_ += 1; 
-        self.bt = t;
-        self.s = v;
-        self.parent = None
-        self.children = []
-        self.touched = False
-    def link(self, child):
-        if child is None:
-            child = ASTNode0("EMPTY", "");
-        child.parent = self;
-        self.children.append(child)
-    def __str__(self):
-        global _depth;
-        tab = "= "*_depth
-        _depth += 1
-        s = self._str(tab)
-        _depth -= 1 
-        return s
-    def _str(self, tab):
-        return tab + f"{self.bt}({self.n}): {self.s}" +"\n" + (((("").join([str(c) for c in self.children]))) if len(self.children) else "")
-
-    def graph(self):
-        gr = f'{self.n} [label="{self.bt}:{self.s}"];\n'
-        for i, c in enumerate(self.children):
-            gr += f'{self.n} -> {str(c.n)} [ label = "{i}" ]\n'
+class TerminationFound(Exception):
+    pass
+class witherParser(object):
+    def __init__(self):
+        self.states = {}
         
-        for c in self.children:
-            gr += c.graph();
-        return gr;
-
-    
-class Parser(object):
-    def __init__(self, text):
-        self.text = text.replace(" ", "").replace("\n", "").replace("\t", "");
-        self.idx  = 0;
-        self.pidx = 0;
-        self.buf  = "";
-    def debug(self, name=None):
-        nl = "\n"
-        if name is not None:
-            print(name)
+    def ingest(self, source):
+        source = list(filter(None, source.split ("\n")))
+        print(f"Processing {len(source)} lines of wither")
+        for line in source:
+            self.ingestLine(line);
+        return self.states
+        
+    def ingestLine(self, line):
+        words = list(filter(None, line.split(" ")))
+        print(f"Processing {len(words)} words of wither {words}")
+        s = words[0]
+        if s in self.states:
+            raise RuntimeError(f"Redeclaration of state {s}!")
+        if words[1] != ":=":
+            raise RuntimeError("Syntax error (expected := after state name)")
+        stateSeries = {0: [], "id":"Any"}#SparseList()
+        self.recurse(words[2:], stateSeries)
+        if 1 not in stateSeries:
+            stateSeries = stateSeries[0]
         else:
-            print("\n\n")
-        print(f"Buffer: {self.buf}{nl}Tape: {repr(self.text[self.idx+self.pidx:])}{nl}Index: {self.idx}{nl}Pindx: {self.pidx} ")
-        #traceback.print_stack()
-        #print("\n\n")
+            stateSeries = [stateSeries]
+        self.states[s] = stateSeries
         
-    def peek(self):
-        idx = self.idx + self.pidx;
-        if idx >= len(self.text):
-            raise RuntimeError("EOF Parsing"); #TODO FIXME more verbose error
-        self.pidx += 1;
-        return self.text[idx];
-    def unpeek(self):
-        if self.pidx < 1:
-            # this should never happen because we don't unpeek if we haven't peeked
-            raise RuntimeError("Internal problem. See comment for details"); 
-        self.pidx -= 1;
-        
-    def poke(self):
-        idx = self.idx + self.pidx;
-        if idx > len(self.text):
-            raise RuntimeError("EOF Parsing"); #TODO FIXME more verbose error
-        self.buf += self.text[idx-1];
-        
-    def consume(self, test):
-        _ = False;
-        while test(self.peek()):
-            _ = True;
-            self.poke()
-        self.unpeek();
-        return _;
-    
-    def test(self, c):
-        if self.peek() == c:
-            self.poke()
-            return True
-        else:
-            self.unpeek()
-            return False
-    
-    def pop(self):
-        tmp = self.buf
-        self.buf = ""
-        self.idx += self.pidx
-        self.pidx = 0
-        return tmp;
-    def drop(self):
-        self.buf = ""
-        self.pidx = 0;
-    def unparse(self, s):
-        if s.s is None:
-            self.debug()
-            raise RuntimeError(f"Unparsing invalid ast node with type {s.bt}")
-        
-        self.idx -= len(s.s)
-        if(self.idx < 0):
-            raise RuntimeError("Index underflow while unparsing");
-
-    def parseConstant(self):
-        # TODO: Return typed value
-        if self.test("0") and self.test("x"):
-            self.consume(lambda x: x in string.hexdigits)
-            return ASTNode0("HEXINT", self.pop())
-
-        self.drop()
-
-        if self.consume(lambda x: x.isdigit()):
-            p1 = self.pop();
-            if self.test("."):
-                if self.consume(lambda x: x.isdigit()):
-                    p1 += self.pop();
-            return ASTNode0("DECINT", p1);
-        self.drop()
-        
-        if self.test('"'):
-            self.consume(lambda x: x != '"')
-            if not self.test('"'):
-                #internal problem, consume has not left us with the trailing " as expected
-                raise RuntimeError("Internal problem. Consult comment");
-            
-                
-            return ASTNode0("STRING", self.pop());
-        
-        self.drop();
-        return None;
-    def parseVarname(self):
-        if not self.peek() in string.ascii_lowercase:
-            self.drop()
-            return None
-        self.poke();
-        self.consume(lambda x: x in (string.ascii_letters + string.digits))
-    
-        return ASTNode0("VARNAME", self.pop())
-         
-        
-    def parseArrayAccess(self):
-        if not self.test("["):
-            return None
-        self.pop()
-        if (rval := self.parseExpression()) is None:
-            self.debug()
-            raise RuntimeError("Exception while parsing: Failed rval on array index")
-        if not self.test("]"):
-            raise RuntimeError("Expected ] after array index")
-        self.pop()
-        arrv = ASTNode0("ARRAY_ACCESS", None);
-        arrv.link(rval)
-        return arrv
-
-    def parseBlock(self):
-        if not self.test("{"):
-            return None
-        self.pop()
-        v = [];
-        block = ASTNode0("BLOCK", None)
-
-        if self.test("^"):
-            self.pop();
-            raise Exception("Unimplemented: Whence Block")
-        elif self.test("?"):
-            self.pop();
-            raise Exception("Unimplemented: Query Block")
-
-        else:
-            pn = None
-            head = None
-            done = False
-            while True:
-                if (node := self.parseContinuation()) is None:
-                    self.debug()
-                    raise RuntimeError("Exception while parsing: failed to parse block")
-                if head is None:
-                    head = node;
-
-                c = self.peek();
-                if c == ';' or c == ',':
-                    self.pop();
-                    dlm = ASTNode0("DROP", None) if c == ';' else ASTNode0("PUSH", None);
-                    node.link(dlm);
-                    if pn is not None:
-                        pn.link(node)
-                    pn = dlm;
-                
+    def recurse(self, tape, series, terminate = "", idx=0):
+        if len(tape) == 0:
+            if terminate != "":
+                raise RuntimeError(f"Expected to reach {terminate} before EOL")
+            return
+        word = tape.pop(0)
+        if word == terminate:
+            raise TerminationFound
+        if word.startswith('"'):
+            series[idx].append({"id":"Match", 0: word.strip('"')})
+        elif word == "?(":
+            subseries = {0: [], "id":"Any"}#SparseList()         
+            try:
+                self.recurse(tape, subseries, terminate=")", idx=0)
+            except TerminationFound:
+                if not 1 in subseries:
+                    subseries = subseries[0]
                 else:
-                    self.unpeek()
-                    if pn is not None:
-                        pn.link(node)
-                    pn = node;
-               
-
-                if self.test("}"):
-                    self.pop();
-                    break;
-
-                
-            block.link(head);
-            return block;
-    def parseOperator(self):
-        l = [
-            "/",
-            "*",
-            "+",
-            "-",
-            "%",
-
-            "&",
-            "|",
-            "^",
-            "~",
-            "!",
-            "=",
-            ">",
-            "<",
-            ]
-        a = self.peek() ; b = self.peek();
-        if (b == '=') and (a in l[-4:]):
-            #tuple
-            op = self.pop()
-            return ASTNode0("OP", op)
-        else:
-            self.unpeek();
-        if a in l:
-            self.pop();
-            return ASTNode0("OP", a)
-
-    def parseEquation(self):
-        if not self.test("("):
-            return None
-        self.pop()
-        if (lv := self.parseExpression()) is None:
-            self.debug()
-            raise RuntimeError("Exception while parsing: failed to parse expression")
-        if (op := self.parseOperator()) is not None:
-            if (rv := self.parseExpression()) is None:
-                self.debug();
-                raise RuntimeError(f"Failed to parse rval for operator {op.s}")
-            
-        if not self.test(")"):
-            raise RuntimeError("Expected ) after equation")
-        eq = ASTNode0("EQ", None)
-        eq.link(lv);
-        eq.link(op);
-        eq.link(rv);
-
-        return eq;
-
-    def parseValue(self):
-        if(node := self.parseVarname()) is not None:
-            pass
-        elif(node := self.parseConstant()) is not None:
-            pass
-        
-        if node is None:
-            return None;
-        
-        if (aa := self.parseArrayAccess()) is not None:
-            aa.link(node)
-            return aa
-        return node
-        
-    def parseInvoke(self):
-        if not self.test("~"):
-            return None
-        self.pop();
-
-        i = ASTNode0("INVOKE", None);
-        if (n := self.parseBlock()) is not None:
-            i.link(n)
-        elif (n := self.parseValue()) is not None:
-            i.link(n)
-        else:
-            self.debug();
-            raise RuntimeError("Failed to parse invoke target!")
-        return i;
-        #TODO: check invokation type at compile time
-
-
-    def parseMagic(self):
-        if self.test("|"):
-            self.pop();
-            return ASTNode0("IO", None);
-        return self.parseInvoke()
-
-           
-
-
-    def parseExpression(self):
-        if (node := self.parseBlock()) is not None:
-            return node
-        if (node := self.parseEquation()) is not None:
-            return node
-        if (node := self.parseValue()) is not None:
-            return node
-        if (node := self.parseMagic()) is not None:
-            return node
-        return None;
-
-
-    def parseContinuation(self):
-        head = None
-        node = None
-        pn = None
-        done = False
-        v = [];
-
-        while True:
-            if (node := self.parseExpression()) is None:
-                self.debug();
-                raise RuntimeError("Expected Expression!")
-            if head is None:
-                head = node   
-            if self.test("-"):
-                if self.test(">"):
-                    self.pop();
-                    ct = ASTNode0("YEET", None);
-                    node.link(ct)
-                    if pn is not None:
-                        pn.link(node)
-                    pn = ct;
-                    continue
-                else:
-                    raise RuntimeError("invalid syntax")
+                    subseries = [subseries]
+                series[idx].append({"id":"Optional", 0:  subseries})
             else:
-                done = True
-        
-            if pn is not None:
-                pn.link(node)
-            pn = node
-            if done:
-                return head
-
-    def parse(self):
-        return self.parseBlock();
-         
+                raise RuntimeError("This should be unreachable?? parsing ?( fail")
+        elif word == "*{":
+            subseries = {0: [], "id":"Any"}#SparseList()#{0: []}
+            try:
+                self.recurse(tape, subseries, terminate="}", idx=0)
+            except TerminationFound:
+                if not 1 in subseries:
+                    subseries = subseries[0]
+                else:
+                    subseries = [subseries]
+                series[idx].append({"id":"Repeat", 0: subseries})
+            else:
+                raise RuntimeError("This should be unreachable?? parsing *{ fail")
+        elif word.isalnum():
+            series[idx].append({"id":"Goto", 0:  word})
+        elif word == "|":
+            idx += 1;
+            series[idx] = []
+        else:
+            raise RuntimeError(f"Unimplemented word {word}")
+        self.recurse(tape, series, terminate=terminate, idx=idx)
--- wither.py
+++ wither.py
@@ -1,13 +1,15 @@
 import json
-
+from parse import witherParser
 src = """
 printable   := "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" | "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | "!" | "#" | "$" | "%" | "&" | "(" | ")" | "*" | "+" | "," | "-" | "." | "/" | ":" | ";" | "<" | "=" | ">" | "?" | "@" | "[" | "\\" | "]" | "^" | "_" | "`" | "{" | "|" | "}" | "~" 
-letter      := "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
+letter      := "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" | "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
 hex         := "a" | "b" | "c" | "d" | "e" | "f"
 digit       := "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
 operator    := "<<" | ">>" | "<=" | ">=" | "!=" | "==" | "/" | "*" | "+" | "-" | "%" | "&" | "|" | "^" | "~" | "!" | "=" | ">" | "<" 
-this        := "%"
+glob        := "%"
+this        := "_"
 sysc        := "^"
+invoke      := "~"
 string      := "'" ?( *{ printable } ) "'"
 integer     := "0x" *{ digit | hex } | digit ?( *{ digit } )
 array       := "[" *{ expression ?( comma ) } "]"
@@ -16,14 +18,14 @@ name        := letter ?( *{ letter | digit } )
 subscript   := "[" expression "]
 equation    := "(" expression ?( operator expression ) ")"
 statement   := expression ?( *{ "->" expression } )
-block       := "{" ?( unglom ) *{ statement ?( semicolon ) } "}"
+block       := "{" ?( unglom ) *{ statement semicolon } "}"
 twople      := expression colon expression
 dict        := "${" *{ twople ?( comma ) } "}"
 filter      := "?{" *{ twople ?( comma ) } "}"
 glom        := "@{" *{ expression ?( comma ) } "}"
-unglom      := "#{" *{ name ?( comma ) } "}"
-expression2 := block | filter | dict | glom | equation | name | constant | this | sysc
-expression  := ?( "~" ) expression2 ?( *{ subscript } ) 
+unglom      := "#{" *{ name ?( comma ) } "}" semicolon
+expression2 := block | filter | dict | glom | unglom | equation | name | constant | glob | this | sysc
+expression  := ?( invoke ) expression2 ?( *{ subscript } ) 
 semicolon   := ";"
 colon       := ":"
 comma       := ","
@@ -32,8 +34,7 @@ file        := *{ statement semicolon }
 """
 
 
-class TerminationFound(Exception):
-    pass
+
 
 class WitherStack(object):
     def __init__(self):
@@ -42,18 +43,19 @@ class WitherStack(object):
     def new_frame(self):
         self.s.insert(0, self.b)
         self.b = []
-        #print(f"@@@@ -> @@@ {json.dumps(self.s)}")
+        print(f"@@@@ -> @@@ {json.dumps(self.s)}")
     def end_frame(self):
-        #print(f"@@@@ <- @@@ {json.dumps(self.b)}")
+        print(f"@@@@ <- @@@ {json.dumps(self.s)}")
+        print(f"            {json.dumps(self.b)}")
         _ = self.b
         self.b = self.s.pop(0)
         return _
     def buffer(self, v):
-        #print(f"@@@@    @@@ {json.dumps(v)}")
         self.b.append(v)
+        print(f"@@@@    @@@ {json.dumps(self.b)}")
     def extend(self, v):
         self.b += v
-        #print(f"@@@@ ## @@@ {json.dumps(self.b)}")
+        print(f"@@@@ ## @@@ {json.dumps(self.b)}")
 class WitherInterpreter(object):
     def __init__(self, machine):
         self.machine = machine
@@ -75,10 +77,10 @@ class WitherInterpreter(object):
                         'filter', 
                         'glom', 
                         'unglom', 
-                         
+                        'invoke',
                         'file']
-        self.drop = [';', '{', '}','(', ')', '[',']', ',', '@{', '${', '?{', ',', ':', '->']
-
+        self.drop = [';', '{', '}','(', ')', '[',']', ',', '@{', '${', '?{', ',', ':', '->', '#{', '~', '%']
+        
     def run(self, source):
         self.tape = source.replace(" ", "").replace("\n", "").replace("\t", "") 
         self.stack.new_frame();
@@ -95,14 +97,15 @@ class WitherInterpreter(object):
 
     def recurse(self, i, sl, tape):
         for s in sl:
-            #print(f"subnode {s}")
             match s["id"]:
                 case "Match":
                     if tape.startswith(s[0]):
-                        #print(f"____ Matched {s[0]} {i} {tape}")
+                        print(f"____ Matched {s[0]} {i} {tape}")
                         tape = tape[len(s[0]):]
                         if not s[0] in self.drop:
-                            self.stack.buffer(s)
+                            self.stack.buffer({
+                                'id':i, 0:s[0]
+                            })
 
                     else:
                         return (False, tape)
@@ -113,7 +116,6 @@ class WitherInterpreter(object):
                     if v:
                         tape = l
                 case "Repeat":
-                    #print(f"Begin matching repeat for {i}")
                     self.stack.new_frame();
                     f = s[0]
                     (v, l) = (True, tape)
@@ -123,11 +125,10 @@ class WitherInterpreter(object):
                             tape = l
                         else:
                             f = self.stack.end_frame()
-                            #print(f"Done matching repeat for {i} {json.dumps(f)}")
                             self.stack.extend(f)
                         
                 case "Goto":
-                    #print(f"Going: {s[0]}")
+                    print(f")(}}{{)( Going: {s[0]} {tape}")
                     self.stack.new_frame()
                     (v, l) = self.recurse(s[0], self.machine[s[0]], tape)
                     if not v:
@@ -136,7 +137,7 @@ class WitherInterpreter(object):
                     else:
                         f = self.stack.end_frame()
                         if s[0] in self.tags:
-                            #print(f")(}}{{)( from {s[0]} {json.dumps(f)}")                            
+                            print(f")(}}{{)( from {s[0]} {json.dumps(f)}")                            
                             self.stack.buffer({"id": s[0], 0: f})
                         else:
                             self.stack.extend(f)
@@ -154,77 +155,9 @@ class WitherInterpreter(object):
                     
                     
 
-class witherParser(object):
-    def __init__(self):
-        self.states = {}
-        
-    def ingest(self, source):
-        source = list(filter(None, source.split ("\n")))
-        print(f"Processing {len(source)} lines of wither")
-        for line in source:
-            self.ingestLine(line);
-        return WitherInterpreter(self.states)
-    def ingestLine(self, line):
-        words = list(filter(None, line.split(" ")))
-        print(f"Processing {len(words)} words of wither {words}")
-        s = words[0]
-        if s in self.states:
-            raise RuntimeError(f"Redeclaration of state {s}!")
-        if words[1] != ":=":
-            raise RuntimeError("Syntax error (expected := after state name)")
-        stateSeries = {0: [], "id":"Any"}#SparseList()
-        self.recurse(words[2:], stateSeries)
-        if 1 not in stateSeries:
-            stateSeries = stateSeries[0]
-        else:
-            stateSeries = [stateSeries]
-        self.states[s] = stateSeries
-        
-    def recurse(self, tape, series, terminate = "", idx=0):
-        if len(tape) == 0:
-            if terminate != "":
-                raise RuntimeError(f"Expected to reach {terminate} before EOL")
-            return
-        word = tape.pop(0)
-        if word == terminate:
-            raise TerminationFound
-        if word.startswith('"'):
-            series[idx].append({"id":"Match", 0: word.strip('"')})
-        elif word == "?(":
-            subseries = {0: [], "id":"Any"}#SparseList()         
-            try:
-                self.recurse(tape, subseries, terminate=")", idx=0)
-            except TerminationFound:
-                if not 1 in subseries:
-                    subseries = subseries[0]
-                else:
-                    subseries = [subseries]
-                series[idx].append({"id":"Optional", 0:  subseries})
-            else:
-                raise RuntimeError("This should be unreachable?? parsing ?( fail")
-        elif word == "*{":
-            subseries = {0: [], "id":"Any"}#SparseList()#{0: []}
-            try:
-                self.recurse(tape, subseries, terminate="}", idx=0)
-            except TerminationFound:
-                if not 1 in subseries:
-                    subseries = subseries[0]
-                else:
-                    subseries = [subseries]
-                series[idx].append({"id":"Repeat", 0: subseries})
-            else:
-                raise RuntimeError("This should be unreachable?? parsing *{ fail")
-        elif word.isalnum():
-            series[idx].append({"id":"Goto", 0:  word})
-        elif word == "|":
-            idx += 1;
-            series[idx] = []
-        else:
-            raise RuntimeError(f"Unimplemented word {word}")
-        self.recurse(tape, series, terminate=terminate, idx=idx)
 
 p = witherParser();
-i = p.ingest(src);
+i = WitherInterpreter(p.ingest(src));
 print(src)
 print(json.dumps(p.states))
 print()
@@ -234,21 +167,53 @@ print()
 
 src = """
 {
+    
     @{a,b} -> ?{ 
         (b==0) : {a;}, 
-        (1): { a % b - > r; @{b,r} -> ~euclid;}
+        (1): { a % b - > r; @{b,r} -> ~e;}
     };
-} -> z;
+} -> e;
 {
-    @{2740, 1760} -> ~euclid;
-} -> main;
-~main;
+    @{2740, 1760} -> ~e;
+} -> m;
+~m;
 """
-
-#src = """
-#'hello world' -> a;
-#a -> b;
-#"""
+src = """
+{
+    #{a, l, h}; 
+    % -> ~p ->{
+        @{a,l,(%-1)} -> ~q;
+        @{d,(%+1),h} -> ~q;
+    };
+} -> q;
+{
+    #{a, i, j};
+    @{a[i], a[j]}
+    -> ~{
+        #{ai, aj}; 
+        aj->a[i]; 
+        ai->a[j];
+    };
+} -> s;
+{
+    #{a, l, h};
+    @{(l - 1), l, a[h]} -> ~{
+        #{i, j, v};
+        _ -> ?{
+            (j < (h-1)):{
+                % -> ?{
+                    (a[j] < v): {
+                        @{a, (i+1), j} -> ~s;
+                        @{(i+1), (j+1), v} -> ~%;                            
+                    }
+                }; 
+            }
+        };
+    };
+    @{a, (i+1), h} -> ~s;
+    (i+1);
+}-> p;
+"""
 print(src)
 
 tree = i.run(src)
